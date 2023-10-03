e4d9972a1942bdb25eebba3d00aa93303c4dfe09
Jonas Steinmejer <jonas04@outlook.dk>
2023-10-03 13:00:57 +0200

    Edited

---
+++ AdminSite/Pages/Billeder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminSite.Pages
+{
+    public class BillederModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ AdminSite/Pages/Køb-Billetter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminSite.Pages
+{
+    public class Køb_BilletterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
