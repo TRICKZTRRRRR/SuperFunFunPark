8bc55f082abfca12d98269baea326b397ff30302
Jonas Steinmejer <jonas04@outlook.dk>
2023-10-05 12:13:26 +0200

    Edited

---
+++ AdminSite/Pages/AdminPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminSite.Pages
+{
+    public class AdminPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
