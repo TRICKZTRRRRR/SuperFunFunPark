049e0ea4b9d15f072c0bf5f72b7b5a434a700f60
Jonas Steinmejer <jonas04@outlook.dk>
2023-10-05 12:48:42 +0200

    Edited

---
+++ AdminSite/Pages/StaffPanel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AdminSite.Pages
+{
+    public class StaffPanelModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
